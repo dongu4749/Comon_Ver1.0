92911bc2ed4f21aa9acc90e3de99585de1a054a6
dongu4749 <dongu4749@naver.com>
2022-06-11 19:46:39 +0900

    Final_Complete

--- Final_Project_3year_Ver1.0/app/src/main/java/se/jbnu/final_project_3year/Fragment/Fragment_Home.java
+++ Final_Project_3year_Ver1.0/app/src/main/java/se/jbnu/final_project_3year/Fragment/Fragment_Home.java
@@ -1,26 +1,19 @@
 package se.jbnu.final_project_3year.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
 import se.jbnu.final_project_3year.HomeActivity;
-import se.jbnu.final_project_3year.Quiz.Quiz_Block_Coding_List_Activity;
-import se.jbnu.final_project_3year.Quiz.Quiz_Multiple_choice_List_Activity;
-import se.jbnu.final_project_3year.Quiz.Quiz_Short_Answer_List_Activity;
 import se.jbnu.final_project_3year.R;
 
 /**
@@ -83,8 +76,7 @@ public class Fragment_Home extends Fragment  {
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.setSupportActionBar(myToolbar);
         activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
-        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 왼쪽 상단 버튼 만들기
-        activity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24);
+
         Button btn_Daily = (Button) v.findViewById(R.id.buttonHomeDaily);
         Button btn_Trend = (Button) v.findViewById(R.id.buttonHomeTrend);
         Button btn_Keyword = (Button) v.findViewById(R.id.buttonHomeKeyword);
@@ -108,45 +100,8 @@ public class Fragment_Home extends Fragment  {
                 ((HomeActivity)getActivity()).replaceFragment(Fragment_Keyword.newInstance());
             }
         });
-
-        drawerLayout = v.findViewById(R.id.drawer_layout);
-        navigationView = v.findViewById(R.id.navigation_view);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id=item.getItemId();
-                if(id==R.id.Quiz_Button){
-                    Intent intent=new Intent(getActivity().getApplicationContext(), Quiz_Multiple_choice_List_Activity.class);
-                    startActivity(intent);
-                }
-                else if(id==R.id.Quiz_typing){
-                    Intent intent=new Intent(getActivity().getApplicationContext(), Quiz_Short_Answer_List_Activity.class);
-                    startActivity(intent);
-                }else if(id==R.id.Quiz_Block){
-                    Intent intent=new Intent(getActivity().getApplicationContext(), Quiz_Block_Coding_List_Activity.class);
-                    startActivity(intent);
-                }
-                else if(id==R.id.Quiz_CloseDrawbar){
-                    //((HomeActivity)getActivity()).replaceFragment(Fragment_Home.newInstance());
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                return true;
-            }
-        });
         return v;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //return super.onOptionsItemSelected(item);
-        switch (item.getItemId()) {
-            case android.R.id.home: {
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
 }
